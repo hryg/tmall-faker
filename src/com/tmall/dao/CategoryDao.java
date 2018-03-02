72698271efab3b0c872b21eddfe039d50c81ddda
Henry Heng <hrygheng@gmail.com>
2018-03-02 20:00:24 +0800

    增加DAO文件夹

---
+++ src/com/tmall/dao/CategoryDao.java
@@ -0,0 +1,11 @@
+package com.tmall.dao;
+
+public class CategoryDao {
+    public int getTotal(){
+        int total = 0;
+
+        String sql  = "select count(*) form category";
+
+        return 0;
+    }
+}
