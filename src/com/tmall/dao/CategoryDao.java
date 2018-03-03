13163ce22007ec9924b91b6eec76eb9f19bc1172
Henry Heng <hrygheng@gmail.com>
2018-03-03 11:58:40 +0800

    增加CategoryDao

--- src/com/tmall/dao/CategoryDao.java
+++ src/com/tmall/dao/CategoryDao.java
@@ -1,11 +1,131 @@
 package com.tmall.dao;
 
+import com.tmall.bean.Category;
+import com.tmall.util.DBUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDao {
-    public int getTotal(){
+
+    public int getTotal() {
         int total = 0;
+        String sql = "select count(*) form category";
 
-        String sql  = "select count(*) form category";
+        try (Connection connection = DBUtil.getConnection();
+             Statement statement = connection.createStatement();) {
 
-        return 0;
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                total = resultSet.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return total;
+    }
+
+    public void add(Category bean) {
+        String sql = "insert into category values(null, ?)";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, bean.getName());
+            statement.execute();
+
+            ResultSet resultSet = statement.getGeneratedKeys();
+            while (resultSet.next()) {
+                bean.setId(resultSet.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Category bean) {
+        String sql = "update category set name = ? where id = ?";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, bean.getName());
+            statement.setInt(1, bean.getId());
+            statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        String sql = "delete from category where id = ?";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Category get(int id) {
+        String sql = "select * from category where id = ?";
+        Category category = null;
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            statement.execute();
+
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                category = new Category();
+                category.setId(resultSet.getInt(1));
+                category.setName(resultSet.getString(2));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return category;
+    }
+
+    public List<Category> list() {
+        return list(0, Short.MAX_VALUE);
+    }
+
+    public List<Category> list(int start, int count) {
+        List<Category> categories = new ArrayList<Category>();
+        String sql = "select * from category order by id desc limit ?, ?";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, start);
+            statement.setInt(2, count);
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Category category = new Category();
+                category.setId(resultSet.getInt(1));
+                category.setName(resultSet.getString(2));
+                categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return categories;
     }
 }
