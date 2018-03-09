26437dd99184d610c7f470f6ca8291cbc8a58d87
Henry Heng <hrygheng@gmail.com>
2018-03-09 17:34:34 +0800

    增加web.xml配置文件

---
+++ src/com/tmall/servlet/BaseBackServlet.java
@@ -0,0 +1,9 @@
+package com.tmall.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class BaseBackServlet extends HttpServlet {
+//    public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+}
