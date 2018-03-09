bbfc366790a8a87b4d53566dc70e02ddf96ebf5f
Henry Heng <hrygheng@gmail.com>
2018-03-09 17:55:26 +0800

    构建基础后台Servlet类

--- src/com/tmall/servlet/BaseBackServlet.java
+++ src/com/tmall/servlet/BaseBackServlet.java
@@ -1,9 +1,32 @@
 package com.tmall.servlet;
 
+import com.tmall.dao.*;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public abstract class BaseBackServlet extends HttpServlet {
-//    public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+    /*
+    public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String delete(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String edit(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String udpate(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String list(HttpServletRequest request, HttpServletResponse response, Page page);
+    */
+
+    protected CategoryDAO categoryDAO = new CategoryDAO();
+    protected OrderDAO orderDAO = new OrderDAO();
+    protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+    protected ProductDAO productDAO = new ProductDAO();
+    protected ProductImageDAO productImageDAO = new ProductImageDAO();
+    protected PropertyDAO propertyDAO = new PropertyDAO();
+    protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+    protected ReviewDAO reviewDAO = new ReviewDAO();
+    protected UserDAO userDAO = new UserDAO();
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+
+    }
 }
