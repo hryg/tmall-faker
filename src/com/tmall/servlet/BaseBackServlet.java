3fc34e8ea16197562690f7e1afda89503b73ee49
Henry Heng <hrygheng@gmail.com>
2018-03-12 14:24:09 +0800

    增加后台Servlet基类，功能待完善

--- src/com/tmall/servlet/BaseBackServlet.java
+++ src/com/tmall/servlet/BaseBackServlet.java
@@ -2,11 +2,17 @@ package com.tmall.servlet;
 
 import com.tmall.dao.*;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public abstract class BaseBackServlet extends HttpServlet {
+    private static final String CLIENT_REDIRECT_PREFIX = "@";
+    private static final String RESPONSE_STRING_PREFIX = "%";
     /*
     public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
     public abstract String delete(HttpServletRequest request, HttpServletResponse response, Page page);
@@ -27,6 +33,38 @@ public abstract class BaseBackServlet extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            // 获取分页信息
+            int start = 0;
+            int count = 5;
+            start = Integer.parseInt(request.getParameter("page.start"));
+            count = Integer.parseInt(request.getParameter("page.count"));
+//            Page page = new Page(start, count);
 
+            // 通过反射，调用对应的方法
+            String method = (String) request.getAttribute("method");
+            Method m = this.getClass().getMethod(method, HttpServletRequest.class, javax.servlet.http.HttpServletResponse.class);
+            String redirect = m.invoke(this, request, response).toString();
+
+            // 根据方法的返回值，进行相应客户端或服务端跳转
+            if (redirect.startsWith(CLIENT_REDIRECT_PREFIX)) {
+                response.sendRedirect(redirect.substring(1));
+            } else if (redirect.startsWith(RESPONSE_STRING_PREFIX)) {
+                response.getWriter().print(redirect.substring(1));
+            } else {
+                request.getRequestDispatcher(redirect).forward(request, response);
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
+
