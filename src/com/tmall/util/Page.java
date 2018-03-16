dfa5848346b9d34512022c9711dd50d58a5d41d7
Henry Heng <hrygheng@gmail.com>
2018-03-16 15:34:50 +0800

    添加Page类

---
+++ src/com/tmall/util/Page.java
@@ -0,0 +1,72 @@
+package com.tmall.util;
+
+public class Page {
+    int start;
+    int count;
+    int total;
+    String param;
+
+    public Page(int start, int count) {
+        this.start = start;
+        this.count = count;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+
+    public int getTotalPage() {
+        int totalPage;
+
+        if (0 == total % count) {
+            totalPage = total / count;
+        } else {
+            totalPage = total / count + 1;
+        }
+
+        if (0 == totalPage) {
+            totalPage = 1;
+        }
+        return totalPage;
+    }
+
+    public int getLast() {
+        int last;
+
+        if (0 == total % count) {
+            last = total - count;
+        } else {
+            last = total - total % count;
+        }
+        last = last < 0 ? 0 : last;
+        return last;
+    }
+}
